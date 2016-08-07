7f64134462fa2ad21a36afa6703aecf8389d5262
KimJongTaek <granknight@gmail.com>
2016-08-08 01:51:51 +0900

    di.factory.Beanfactory working

--- src/main/java/core/di/factory/BeanFactory.java
+++ src/main/java/core/di/factory/BeanFactory.java
@@ -1,12 +1,19 @@
 package core.di.factory;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import org.reflections.ReflectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.collect.Maps;
+import org.springframework.beans.BeanUtils;
 
 public class BeanFactory {
 	private static final Logger logger = LoggerFactory.getLogger(BeanFactory.class);
@@ -17,6 +24,55 @@ public class BeanFactory {
 
 	public BeanFactory(Set<Class<?>> preInstanticateBeans) {
 		this.preInstanticateBeans = preInstanticateBeans;
+
+		preInstanticateBeans.stream().forEach(i -> {
+            try {
+                recursiveByNoConstructorClass(i);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            }
+
+        });
+
+	}
+
+	private Object recursiveByNoConstructorClass(Class<?> clazz) throws IllegalAccessException, InstantiationException {
+
+		Object obj = beans.get(clazz.getDeclaringClass());
+
+		if(obj != null)
+			return obj;
+
+        Constructor[] constructors = clazz.getConstructors();
+        for(Constructor constructor : constructors){
+            if(constructor.getParameterTypes().length>0){
+
+                Set<Method> methods = ReflectionUtils.getAllMethods(clazz, ReflectionUtils.withAnnotation(core.annotation.Inject.class));
+
+                for(Method method : methods){
+                    Class<?>[] parameterClasses = method.getParameterTypes();
+
+                    for(Class<?> parameter : parameterClasses){
+                        if(beans.get(parameter) == null){
+                            recursiveByNoConstructorClass(clazz);
+                        }
+                    }
+
+
+                    //beans.put(clazz.getDeclaringClass(), BeanUtils.instantiateClass(constructor, beans.get));
+
+                }
+
+            }else{
+                beans.put(clazz.getDeclaringClass(), clazz.newInstance());
+            }
+        }
+
+
+
+        return obj;
 	}
 	
 	@SuppressWarnings("unchecked")
---
+++ src/test/java/core/nmvc/ReflectionsTest.java
@@ -0,0 +1,99 @@
+package core.nmvc;
+
+import com.google.common.base.Predicate;
+import core.annotation.RequestMapping;
+import org.junit.Before;
+import org.junit.Test;
+import org.reflections.Configuration;
+import org.reflections.ReflectionUtils;
+import org.reflections.Reflections;
+import org.reflections.adapters.MetadataAdapter;
+import org.reflections.scanners.MethodAnnotationsScanner;
+import org.reflections.scanners.MethodParameterScanner;
+import org.reflections.scanners.Scanner;
+import org.reflections.serializers.Serializer;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.Nullable;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+
+/**
+ * Created by granknight on 2016. 8. 1..
+ */
+public class ReflectionsTest {
+
+    public static final Logger log = LoggerFactory.getLogger(ReflectionsTest.class);
+
+    Reflections reflections;
+
+//    Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(core.annotation.Controller.class);
+//
+//    for(Class controller : controllers){
+//
+//        Set<Method> methods = reflections.getMethodsAnnotatedWith(core.annotation.RequestMapping.class);
+//
+//        //Set<Class<?>> annotations = reflections.getTypesAnnotatedWith(core.annotation.RequestMapping.class);
+//
+//
+//
+//    }
+
+    @Before
+    public void setUp() throws Exception{
+        reflections = //new Reflections("core.nmvc");
+            new Reflections(new ConfigurationBuilder().setUrls(ClasspathHelper.forPackage("core.nmvc")).addScanners(new MethodAnnotationsScanner(), new MethodParameterScanner()));
+
+    }
+
+    @Test
+    public void getControllerAnnotationControllers() throws Exception{
+        Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(core.annotation.Controller.class);
+
+        for(Class clazz : controllers){
+            log.debug("Controller Annotation : " + clazz);
+        }
+
+    }
+
+    @Test
+    public void getRequestMappingAnnotationMethods() throws Exception{
+        Set<Method> methods = reflections.getMethodsAnnotatedWith(core.annotation.RequestMapping.class);
+
+        for(Method method : methods){
+            log.debug("Method Annotation : " + method);
+        }
+    }
+
+    @Test
+    public void getAnnotationWithValue() throws Exception{
+        Set<Method> methods = reflections.getMethodsAnnotatedWith(core.annotation.RequestMapping.class);
+
+        for(Method method : methods){
+            log.debug("Method Annotation : " + method);
+
+            RequestMapping requestMapping = method.getAnnotation(core.annotation.RequestMapping.class);
+
+            log.debug("value : {}", requestMapping.value());
+
+            log.debug("method : {}", requestMapping.method());
+
+        }
+    }
+
+
+    @Test
+    public void reflectionUtilTest() throws Exception{
+        Set<Method> methodSet = ReflectionUtils.getAllMethods(MyController.class, ReflectionUtils.withAnnotation(core.annotation.RequestMapping.class));
+
+
+
+    }
+
+}
