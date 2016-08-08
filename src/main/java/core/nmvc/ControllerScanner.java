37dd774e7f805920b6550ff758c7136bf764d9bb
KimJongTaek <granknight@gmail.com>
2016-08-08 13:04:12 +0900

    handler Execution ing...

---
+++ src/main/java/core/nmvc/ControllerScanner.java
@@ -0,0 +1,19 @@
+package core.nmvc;
+
+import java.util.Set;
+
+/**
+ * Created by aart on 2016-08-03.
+ */
+public class ControllerScanner {
+
+    public ControllerScanner(Object... basePackage){
+
+    }
+
+    public Set<?> getControllers(){
+        return null;
+    }
+
+
+}
